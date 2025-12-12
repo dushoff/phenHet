f2e6db9d8b7bc9b27127f79b1f2eb687083542b4
Jonathan Dushoff <dushoff@mcmaster.ca>
2025-12-12 09:13:30 -0500

    new file:   slow/big.post.rds

--- big.params.R
+++ big.params.R
@@ -1,7 +1,7 @@
 D <- 10 ## Length of the disease generation (in reporting periods)
-rho <- 1 ## β/γ
-N <- 1e5
-kappa <- 1 ## Excess CV, 0 for Poisson
+rho <- 1.0 ## β/γ
+N <- 1e6
+kappa <- 0 ## Excess CV, 0 for Poisson
 lambda <- 5
-seed <- 2639
+seed <- 2641
 MaxTime <- 4e4
