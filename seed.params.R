b442d1fc277c7c19c58292ba9eb997175edd9a6b
Jonathan Dushoff <dushoff@mcmaster.ca>
2025-12-12 10:04:27 -0500

    Living room

--- seed.params.R
+++ seed.params.R
@@ -1,7 +1,7 @@
 N <- 1e7
-D <- 10 ## Length of the disease generation (in reporting periods)
+D <- 10 ## Number of reporting periods per disease generation
 rho <- 1 ## β/γ
 kappa <- 1 ## Excess CV, 0 for Poisson
 lambda <- 5
-seed <- 2639
+seed <- 26
 MaxTime <- 4e4
